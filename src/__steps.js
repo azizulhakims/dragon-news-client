25637b90a85ecedf83994a104ebf2fe799cd9260
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-18 18:35:31 +0600

    add

--- src/__steps.js
+++ src/__steps.js
@@ -1,5 +1,9 @@
 /*
 1. create firebase app
 2. get firebase configuration
-3. 
+3. make sure to export app from firebase config
+4. create a react app
+5. install firebase and react route dom
+5. install bootstrap and react-bootstrap
+6. make sure to import bootstrap css in the index.js file
 */
