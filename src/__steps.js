1d6c753069c884ef402446de4158ac32f8fb60f7
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-19 00:06:27 +0600

    add pages

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,6 @@
 /*
+Basic project setup
+--------------
 1. create firebase app
 2. get firebase configuration
 3. make sure to export app from firebase config
@@ -6,4 +8,9 @@
 5. install firebase and react route dom
 5. install bootstrap and react-bootstrap
 6. make sure to import bootstrap css in the index.js file
+
+*/
+
+/*
+Router Setup
 */
